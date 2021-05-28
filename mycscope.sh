08b7696ece02c2876b0a8d94d088336aac1741a5
BinZlP <dfafqfdaes159@gmail.com>
2021-05-28 16:47:47 +0900

    search .ipp files

--- mycscope.sh
+++ mycscope.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find . \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.hpp' -o -name '*.s' -o -name '*.ic' -o -name '*.s'  \) -print > cscope.files
+find . \( -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.hpp' -o -name '*.ipp' -o -name '*.s' -o -name '*.ic' -o -name '*.s'  \) -print > cscope.files
 cscope -i cscope.files
